e64c4564337274a423acc24a869ed243b2254bcd
andrijanakastratovic011-oss <andrijana.kastratovic011@gmail.com>
2026-05-15 16:24:35 +0200

    start analysing data

---
+++ f1_startingAnalysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+dataset=pd.read_csv('dataEngineeringDataset.csv')
+dataset.info()
+print(dataset.isnull().sum())
+print(dataset.describe())
+print(dataset.duplicated())
+for col in dataset.select_dtypes(include='object').columns:
+    print(f"\n--- {col} ---")
+    print(dataset[col].unique())
+
